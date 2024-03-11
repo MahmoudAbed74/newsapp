1e0d7f0aa0dc7de42d7bd5b699f80115a4e572c8
Mahmoud Abed <mahmoudabed120@gmail.com>
2024-03-11 02:31:49 +0200

    Rest API

--- lib/services/news_Service.dart
+++ lib/services/news_Service.dart
@@ -6,7 +6,7 @@ class news_service {
   final Dio dio;
   const news_service(this.dio);
 
-  getGeneralNews() async {
+  Future<List<ArticalModel>> getGeneralNews() async {
     Response response = await dio.get(
         "https://newsapi.org/v2/top-headlines?apiKey=5e3369148b34434a8deea22017d900d0&country=eg&category=general");
     Map<String, dynamic> jsonData = response.data;
@@ -18,6 +18,19 @@ class news_service {
             title: e["title"],
             subTitle: e["description"]))
         .toList();
+
+    //ToDO another method
+    // List<ArticalModel> articalList = [];
+
+    // for (var article in articles) {
+    //   ArticalModel articalModel = ArticalModel(
+    //       img: article['urlToImage'],
+    //       title: article["title"],
+    //       subTitle: article["description"]);
+    //   articalList.add(articalModel);
+    // }
+
     print(articalList);
+    return articalList;
   }
 }
